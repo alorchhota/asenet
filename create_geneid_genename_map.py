ddf5016c063b1502c6d3755fe0b183cca36da8d0
Ashis Saha <alorchhota@gmail.com>
2014-09-11 02:01:08 -0400

    gencode processed file creation uncommented.

--- create_geneid_genename_map.py
+++ create_geneid_genename_map.py
@@ -1,22 +1,26 @@
 import gtf
 import pandas as pd
+import os
 
-# gencode_annotation_src = 'data/gencode.v19.annotation.gtf'
-# gr = gtf.GencodeGTFReader(gencode_annotation_src, False)
-# print(len(gr.data))
-# #print(gr.data.ix[0:4])
-# processed_src = 'results/processed.txt'
-# gr.save(processed_src)
-#
+home_dir = '/home/asaha6/github/asenet'
 
-processed_src = '/home/ashis/work/github/asenet/data/gencode.v19.annotation.processed.gtf' 
-gr2 = gtf.GencodeGTFReader(processed_src, processed=True)
-# print(len(gr.data))
-# #print(gr.data.ix[0:4])
+# set working directory
+os.chdir(home_dir)
 
-dic = gr2.map_geneid_to_genename()
-print(dic['ENSG00000223972'])
+gencode_annotation_src = 'data/gencode.v19.annotation.gtf'
+gr = gtf.GencodeGTFReader(gencode_annotation_src, False)
+print(len(gr.data))
+#print(gr.data.ix[0:4])
+processed_src = 'results/gencode.v19.annotation.processed.gtf'
+gr.save(processed_src)
 
-df = pd.DataFrame.from_items([('gene_id',dic.keys()),('gene_name',dic.values())])
-df.to_csv(path_or_buf='results/genemap.txt', sep='\t', header=True)
+
+#processed_src = '/home/ashis/work/github/asenet/data/gencode.v19.annotation.processed.gtf' 
+#gr2 = gtf.GencodeGTFReader(processed_src, processed=True)
+
+#dic = gr2.map_geneid_to_genename()
+#print(dic['ENSG00000223972'])
+
+#df = pd.DataFrame.from_items([('gene_id',dic.keys()),('gene_name',dic.values())])
+#df.to_csv(path_or_buf='results/genemap.txt', sep='\t', header=True)
 
