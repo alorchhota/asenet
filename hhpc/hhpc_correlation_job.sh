4270be0a8357f6bf1954c15162560c827964fff9
Ashis Saha <asaha6@login.(none)>
2014-09-15 11:42:54 -0400

    added scripts to run jobs in hhpc cluster

---
+++ hhpc/hhpc_correlation_job.sh
@@ -0,0 +1,2 @@
+cd /home/asaha6/github/asenet
+/home/asaha6/programs/anaconda/bin/python  calculate_tf_ase_correlations.py > results/out.txt
---
+++ hhpc/run_corr.sh
@@ -0,0 +1,2 @@
+cd /home/asaha6/github/asenet
+qsub -l walltime=5:00:00 hhpc_correlation_job.sh > results/out.txt
