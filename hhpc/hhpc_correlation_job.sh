ca07f648e67bee2138f495927ad0423103694a0b
Ashis Saha <asaha6@login.(none)>
2014-09-16 09:31:56 -0400

    run script creates output filenames using current time.

--- hhpc/hhpc_correlation_job.sh
+++ hhpc/hhpc_correlation_job.sh
@@ -1,2 +1,3 @@
 cd /home/asaha6/github/asenet
-/home/asaha6/programs/anaconda/bin/python  calculate_tf_ase_correlations.py > results/out.txt
+outFile='results/out_'$(date +%Y-%m-%d_%H-%M-%S)'.txt'
+/home/asaha6/programs/anaconda/bin/python  calculate_tf_ase_correlations.py > $outFile
