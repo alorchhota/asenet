f0d035a1916d3569708fba9453b841a7fe87f3cc
Ashis Saha <alorchhota@gmail.com>
2014-12-19 09:38:39 -0500

    new run.

--- multipletests_correction.py
+++ multipletests_correction.py
@@ -5,13 +5,13 @@ import numpy as np
 
 ''' setting variables '''
 home_dir = '/home/asaha6/github/asenet'
-test_statistics_path = 'results/test_statistics_pseudo_2014-12-17_15-55-45.txt'
+test_statistics_path = 'results/test_statistics_pseudo_2014-12-19_00-02-51.txt'
 
 # method = 'fdr_bh' or 'bonferroni'
 method_param = 'fdr_bh'
 alpha = 0.05;
 
-multitest_corrected_test_statistics_dest_path = 'results/test_statistics_pseudo_2014-12-17_15-55-45_' + method_param + '.txt'
+multitest_corrected_test_statistics_dest_path = 'results/test_statistics_pseudo_pseudo_2014-12-19_00-02-51_' + method_param + '.txt'
 
 
 # set working directory
