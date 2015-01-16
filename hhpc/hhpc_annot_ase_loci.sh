a1232ef0aa0d3460754c8464010fd2410870c15b
alorchhota <alorchhota@gmail.com>
2015-01-16 17:43:10 -0500

    annotation contains pseudogene status.

--- hhpc/hhpc_annot_ase_loci.sh
+++ hhpc/hhpc_annot_ase_loci.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-qsub -l walltime=10:00:00 annot_ase_loci.sh > hhpc_annot_ase_loci.txt
+qsub -l walltime=10:00:00 annot_ase_loci.sh > hhpc_out_annot_ase_loci.txt
+
--- map_ase_locus_to_gene.py
+++ map_ase_locus_to_gene.py
@@ -36,17 +36,22 @@ import gtf
 
 print('reading gencode annotations ...')
 gen_annot = gtf.GencodeGTFReader(gencode_annot_path, processed=True)
-gen_annot.data = gen_annot.data.loc[:, ['chr', 'feature', 'start', 'end', 'gene_name']]
+gen_annot.data = gen_annot.data.loc[:, ['chr', 'feature', 'start', 'end', 'gene_name', 'gene_type']]
 
 print('reading dgn ase locus data ...')
 ase_loc_ann_data = pd.read_table(ase_loc_path, sep=' ', header=0, index_col=None)
-
-
 n_loci = ase_loc_ann_data.shape[0]
+
+
+def isPseudogene(gene_type):
+    return(gene_type.endswith('pseudogene'))
+
+def containsPseudogene(gene_types):
+    return(any([isPseudogene(gt) for gt in gene_types]))
 
 # list to store annotations of all loci
 annotation = ['' for _ in range(n_loci)]
-
+pseudoStatus = [False for _ in range(n_loci)]
 n_chr = 23
 
 for chr in range(1, n_chr+1):
@@ -70,27 +75,35 @@ for chr in range(1, n_chr+1):
         matched_index = np.where(matched)[0]
         matched_entities = chr_annotdata.iloc[matched_index, :]
         gene_names = set(matched_entities.loc[:, 'gene_name'])
+        gene_types = set(matched_entities.loc[:, 'gene_type'])
         #
         # if multiple genes mapped, take the expressed one
         gene_name = ''
+        ps = False
         if len(gene_names) == 0:
             gene_name = '-'
         elif len(gene_names) == 1:
             gene_name = gene_names.pop()
+            ps = isPseudogene(matched_entities.iloc[0, 5])
+            if len(gene_types) > 1:
+                print('Multiple gene types: ' + str(ld[0]))
         else:
             is_exon = matched_entities.loc[:, 'feature'] == 'exon'
             if sum(is_exon) > 0:
                 exon_index = np.where(is_exon)[0]
                 gene_name = matched_entities.iloc[exon_index[0], 4] # take first gene_name
+                ps = isPseudogene(matched_entities.iloc[exon_index[0], 5])
             else:
-                gene_name = gene_names.pop()
+                gene_name = matched_entities.iloc[0, 4] # take first gene_name
+                ps = isPseudogene(matched_entities.iloc[0, 5])
         #
         # store annotations
         annotation[ld[0]] = gene_name
+        pseudoStatus[ld[0]] = ps
 
 print('saving annotations into file:' + annot_dest_path + ' ...')
 with open(annot_dest_path, 'w') as fh:
-    text = '\n'.join(annotation)
+    text = '\n'.join([pair[0] + '\t' + str(int(pair[1])) for pair in zip(annotation, pseudoStatus)])
     fh.write(text)
 
 print('Done!' )
